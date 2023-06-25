7288979f7665830fa12c227a5aa964bb81326192
Fraggod <102302326+Fraggod@users.noreply.github.com>
2023-06-25 22:38:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,98 +1,87 @@
-import os
 import logging
 import datetime
-from telegram import Update
+from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
 
-# Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# Retrieve the bot token from the environment variable
 bot_token = os.environ.get('BOT_TOKEN')
+log_group_id = os.environ.get('LOG_GROUP_ID')
 
-# Retrieve the approved user IDs from the environment variable
-approved_user_ids = set(int(user_id) for user_id in os.environ.get('APPROVED_USER_IDS', '').split(','))
+approved_user_ids_str = os.environ.get('APPROVED_USER_IDS')
+approved_user_ids = [int(user_id.strip()) for user_id in approved_user_ids_str.split(',')] if approved_user_ids_str else []
+
+# Configure logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Start command handler
 def start_command(update: Update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the bot!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the subscription bot!")
 
 # Paid command handler
 def paid_command(update: Update, context):
     if update.message.reply_to_message is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to a message to process the payment.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to a user's message to process the payment.")
         return
 
     replied_user = update.message.reply_to_message.from_user
     user_id = replied_user.id
     username = replied_user.username
     first_name = replied_user.first_name
+    message_text = update.message.text.strip().split()
+
+    if len(message_text) < 2:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please specify the payment amount and validity period.")
+        return
+
+    payment_amount = ''.join(filter(str.isdigit, message_text[1]))  # Extract the payment amount
+
+    # Extract the validity period from the message
+    validity_period = 1  # Default validity period is 1 day
+    for word in message_text[2:]:
+        if word.isdigit():
+            validity_period = int(word)
+            break
 
     if update.message.from_user.id in approved_user_ids:
-        message_text = update.message.text.split()
-        payment_amount = ''.join(filter(str.isdigit, message_text[1])) if len(message_text) > 1 else None
-        expire_days = ''.join(filter(str.isdigit, message_text[2])) if len(message_text) > 2 else '1'
-        current_date = datetime.datetime.now().date()
-        expire_date = current_date + datetime.timedelta(days=int(expire_days))
+        output_message = "THANKS FOR YOUR SUBSCRIPTION\n"
+        output_message += f"User ID: {user_id}\n\n"
 
-        if username is not None:
-            user_info = f"Username: @{username}"
-        else:
-            user_info = f"First Name: {first_name}"
+        if username:
+            output_message += f"Username: @{username}\n\n"
+        elif first_name:
+            output_message += f"First Name: {first_name}\n\n"
 
-        output_message = f"THANKS FOR YOUR SUBSCRIPTION\n" \
-                         f"User ID: {user_id}\n" \
-                         f"{user_info}\n" \
-                         f"Amount: {payment_amount}\n" \
-                         f"Subscription Valid till: {expire_date}"
+        current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+        expire_date = (datetime.datetime.now() + datetime.timedelta(days=validity_period)).strftime("%Y-%m-%d")
 
-        # Log the output message to the log group
+        output_message += f"Amount: {payment_amount} USD\n"
+        output_message += f"Subscription Start: {current_date}\n"
+        output_message += f"Valid Till: {expire_date}"
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text=output_message)
+
+        # Log the output message
         logger.info(output_message)
 
         # Send the log message to the log group
-        log_group_id = os.environ.get('LOG_GROUP_ID')
-        context.bot.send_message(chat_id=log_group_id, text=output_message)
+        bot = Bot(token=bot_token)
+        bot.send_message(chat_id=log_group_id, text=output_message)
     else:
         context.bot.send_message(chat_id=update.effective_chat.id, text="You are not an approved user.")
-
-# Profile command handler
-def profile_command(update: Update, context):
-    if update.message.reply_to_message is None:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to a message to view the profile.")
-        return
-
-    replied_user = update.message.reply_to_message.from_user
-    user_id = replied_user.id
-
-    if update.message.from_user.id in approved_user_ids:
-        # Check if the user has an active subscription by searching for their user ID in the log group messages
-        log_group_id = os.environ.get('LOG_GROUP_ID')
-        chat_messages = context.bot.get_chat(chat_id=log_group_id).get('messages')
-        subscription_messages = [message for message in chat_messages if str(user_id) in message.get('text', '')]
-
-        if subscription_messages:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="The user has an active subscription.")
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="The user does not have an active subscription.")
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You are not an approved user.")
-
-# Error handler
-def error_handler(update: Update, context):
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 # Create the Telegram bot
-updater = Updater(bot_token, use_context=True)
-dispatcher = updater.dispatcher
+bot = Bot(token=bot_token)
+updater = Updater(bot=bot, use_context=True)
 
-# Register the command handlers
-dispatcher.add_handler(CommandHandler("start", start_command))
-dispatcher.add_handler(CommandHandler("paid", paid_command))
-dispatcher.add_handler(CommandHandler("profile", profile_command))
-
-# Register the error handler
-dispatcher.add_error_handler(error_handler)
+# Register handlers
+start_handler = CommandHandler('start', start_command)
+paid_handler = CommandHandler('paid', paid_command)
+updater.dispatcher.add_handler(start_handler)
+updater.dispatcher.add_handler(paid_handler)
 
 # Start the bot
 updater.start_polling()
+
+# Run the bot until it is stopped manually
+updater.idle()
