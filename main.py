3e6c5b6c105b4fda5d3a01cd6182b61981d199d3
Fraggod <102302326+Fraggod@users.noreply.github.com>
2023-06-25 18:07:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,79 +1,42 @@
-import os
-import psycopg2
-from telethon import TelegramClient, events
+from telegram import Bot, Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-# Telegram API credentials
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
-bot_token = os.environ.get('BOT_TOKEN')
+# Telegram bot token
+bot_token = '5944836678:AAFEFzsbnOrNuescpuhc2rgX9pb4bJJEytM'
 
-# Database credentials
-db_url = os.environ.get('DATABASE_URL')
+# List of approved users
+approved_users = ['tiny_pro', 'FRAG_GOD_HACKER']  # Add the usernames of approved users here
 
-# Owner username
-owner_username = "your_owner_username"  # Replace with your own owner username
+# Start command handler
+def start_command(update: Update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Welcome to the subscription bot!")
 
-# Initialize the Telegram client
-client = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+# Paid command handler
+def paid_command(update: Update, context):
+    user = update.message.reply_to_message.from_user
+    username = user.username
+    user_id = user.id
+    message_text = update.message.text.strip().split()
+    payment_amount = ''.join(filter(str.isdigit, message_text[1]))  # Extract the payment amount
 
-# Connect to the PostgreSQL database
-conn = psycopg2.connect(db_url)
-cursor = conn.cursor()
-
-# /start command handler
-@client.on(events.NewMessage(pattern='/start'))
-async def start_command(event):
-    await event.respond('Welcome to the bot!')
-
-# /profile command handler
-@client.on(events.NewMessage(pattern='/profile'))
-async def profile_command(event):
-    user_id = event.sender_id
-    has_subscription, days_left = check_subscription(user_id)
-
-    if has_subscription:
-        await event.respond(f"You have {days_left} days left in your subscription.")
+    if username in approved_users:
+        output_message = f"Customer - {username} (user id: {user_id}) has paid {payment_amount}."
+        context.bot.send_message(chat_id=update.effective_chat.id, text=output_message)
     else:
-        await event.respond("Your subscription has expired or you are not a subscriber.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You are not an approved user.")
 
-# /sub command handler (for the bot owner)
-@client.on(events.NewMessage(pattern='/sub'))
-async def sub_command(event):
-    sender_entity = await event.client.get_input_entity(event.sender_id)
-    sender_username = sender_entity.username
+# Create the Telegram bot
+bot = Bot(token=bot_token)
+updater = Updater(bot=bot, use_context=True)
 
-    if sender_username and sender_username.lower() == owner_username.lower():
-        command_args = event.raw_text.split()
-        if len(command_args) == 3:
-            user_id = event.reply_to_msg.sender_id.user_id
-            days = command_args[1]
-            amount_paid = command_args[2]
+# Register handlers
+start_handler = CommandHandler('start', start_command)
+paid_handler = CommandHandler('paid', paid_command)
+updater.dispatcher.add_handler(start_handler)
+updater.dispatcher.add_handler(paid_handler)
 
-            store_subscription(user_id, days, amount_paid)
+# Start the bot
+updater.start_polling()
 
-            message = f"{user_id} thanks for subscribing to our bot. Your plan is valid for {days} days. Amount paid: {amount_paid}."
-            await event.respond(message)
-    else:
-        await event.respond("You are not authorized to use this command.")
-
-# Function to check user subscription
-def check_subscription(user_id):
-    cursor.execute("SELECT days FROM subscriptions WHERE user_id = %s;", (str(user_id),))
-    result = cursor.fetchone()
-
-    if result is not None:
-        days_left = result[0]
-        has_subscription = True
-    else:
-        days_left = 0
-        has_subscription = False
-
-    return has_subscription, days_left
-
-# Function to store user subscription
-def store_subscription(user_id, days, amount_paid):
-    cursor.execute("INSERT INTO subscriptions (user_id, days, amount_paid) VALUES (%s, %s, %s);", (user_id, days, amount_paid))
-    conn.commit()
-
-# Start the Telegram client
-client.run_until_disconnected()
+# Run the bot until it is stopped manually
+updater.idle()
