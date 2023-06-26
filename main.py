990ff1a8c506f178216890fdbbbd1de0dabc5202
Fraggod <102302326+Fraggod@users.noreply.github.com>
2023-06-26 06:54:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import os
 import logging
 import datetime
+import os
 from telegram import Bot, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+# Load environment variables
+bot_token = os.getenv("BOT_TOKEN")
+log_group_id = os.getenv("LOG_GROUP_ID")
+approved_user_ids = os.getenv("APPROVED_USER_IDS", "").split(",")
+
 # Rest of the code...
 
-# Telegram bot token
-bot_token = os.environ.get('BOT_TOKEN')
 
-# Log group ID
-log_group_id = os.environ.get('LOG_GROUP_ID')
-
-# List of approved user IDs
-approved_user_ids = list(map(int, os.environ.get('APPROVED_USER_IDS', '').split(',')))
+# Dictionary to store subscription data
+subscriptions = {}
 
 # Configure logging
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -72,78 +72,72 @@ def paid_command(update: Update, context):
         # Send the log message to the log group
         bot = Bot(token=bot_token)
         bot.send_message(chat_id=log_group_id, text=output_message)
+
+        # Store the subscription data in the dictionary
+        subscriptions[user_id] = {
+            'payment_amount': payment_amount,
+            'validity_period': validity_period,
+            'start_date': current_date,
+            'expire_date': expire_date
+        }
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You are not an approved user.")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="You are not authorized to use this command.")
 
 # Profile command handler
 def profile_command(update: Update, context: CallbackContext):
-    approved_users = os.getenv("APPROVED_USERS").split(",")
-    user_id = update.effective_user.id
-
-    if user_id not in approved_users:
-        update.message.reply_text("You are not authorized to use this command.")
+    if update.message.reply_to_message is None:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please reply to a user's message to check the profile.")
         return
 
-    replied_user = update.message.reply_to_message.from_user
-    replied_user_id = replied_user.id
+    replied_user_id = update.message.reply_to_message.from_user.id
 
-    # Check if replied user has an active subscription
-    if replied_user_id not in logger.buffer:
-        update.message.reply_text("No active subscription found for the user.")
-        return
+    if replied_user_id in subscriptions:
+        subscription = subscriptions[replied_user_id]
+        output_message = f"Subscription Data for User ID: {replied_user_id}\n\n"
+        output_message += f"Payment Amount: {subscription['payment_amount']} USD\n"
+        output_message += f"Validity Period: {subscription['validity_period']} days\n"
+        output_message += f"Subscription Start: {subscription['start_date']}\n"
+        output_message += f"Valid Till: {subscription['expire_date']}"
+    else:
+        output_message = "No subscription data found for the replied user."
 
-    records = logger.buffer[replied_user_id]
-    current_time = datetime.datetime.now()
-
-    for record in records:
-        valid_till = datetime.datetime.strptime(record["valid_till"], "%Y-%m-%d")
-
-        if valid_till >= current_time:
-            update.message.reply_text(f"User has an active subscription until {valid_till}.")
-            return
-
-    update.message.reply_text("User's subscription has expired.")
-
-# Rest of the code...
+    context.bot.send_message(chat_id=update.effective_chat.id, text=output_message)
 
 # Check data command handler
-def check_data_command(update: Update, context):
-    # Check if the user is an approved user
-    if update.message.from_user.id in approved_user_ids:
-        data = ''
-        for record in logger.handlers[0].buffer:
-            data += record + '\n'
-        if data:
-            context.bot.send_message(chat_id=update.effective_chat.id, text=data)
-        else:
-            context.bot.send_message(chat_id=update.effective_chat.id, text="No data available.")
+def check_data_command(update: Update, context: CallbackContext):
+    output_message = "Subscription Data:\n\n"
+    if subscriptions:
+        for user_id, subscription in subscriptions.items():
+            output_message += f"User ID: {user_id}\n"
+            output_message += f"Payment Amount: {subscription['payment_amount']} USD\n"
+            output_message += f"Validity Period: {subscription['validity_period']} days\n"
+            output_message += f"Subscription Start: {subscription['start_date']}\n"
+            output_message += f"Valid Till: {subscription['expire_date']}\n\n"
     else:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="You are not an approved user.")
+        output_message += "No subscription data found."
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=output_message)
 
 # Error handler
-def error(update: Update, context):
+def error(update: Update, context: CallbackContext):
     logger.warning(f"Update {update} caused error {context.error}")
 
 def main():
-    # Create the Telegram Updater and pass in the bot's token
-    updater = Updater(bot_token)
-
-    # Get the dispatcher to register handlers
+    updater = Updater(token=bot_token, use_context=True)
     dispatcher = updater.dispatcher
 
-    # Register command handlers
+    # Add command handlers
     dispatcher.add_handler(CommandHandler("start", start_command))
     dispatcher.add_handler(CommandHandler("paid", paid_command))
     dispatcher.add_handler(CommandHandler("profile", profile_command))
     dispatcher.add_handler(CommandHandler("check_data", check_data_command))
 
-    # Register error handler
+    # Add error handler
     dispatcher.add_error_handler(error)
 
-    # Start the bot
     updater.start_polling()
+    logger.info("Bot started polling.")
 
-    # Run the bot until Ctrl-C is pressed
     updater.idle()
 
 if __name__ == '__main__':
