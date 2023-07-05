407185829c831d0d4634e476d551e4dbb3eacb27
Fraggod <102302326+Fraggod@users.noreply.github.com>
2023-07-05 16:46:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -211,11 +211,12 @@ def get_expired_subscriptions(connection):
     cursor = connection.cursor()
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
     cursor.execute("""
-        SELECT user_id, message FROM logs WHERE message LIKE 'Valid Till: %' AND message <= %s;
+        SELECT user_id, message FROM logs WHERE message LIKE 'Valid Till: %%' AND message < %s;
     """, (current_date,))
     result = cursor.fetchall()
     cursor.close()
     return result
+
 
 if __name__ == '__main__':
     main()
